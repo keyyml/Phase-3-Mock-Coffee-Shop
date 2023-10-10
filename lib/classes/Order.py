9df48af948b1df9662b46442c11a34b88a71a5c6
Eleanor Kelman <ekelman3@gmail.com>
2023-10-10 09:09:46 -0400

    set up mock CC

---
+++ lib/classes/Order.py
@@ -0,0 +1,5 @@
+class Order:
+    def __init__(self, customer, coffee, price):
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
---
+++ lib/debug.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import ipdb
+
+from classes.Coffee import Coffee
+from classes.Customer import Customer
+from classes.Order import Order
+
+if __name__ == '__main__':
+    print("HELLO! :) let's debug")
+
+    ipdb.set_trace()
